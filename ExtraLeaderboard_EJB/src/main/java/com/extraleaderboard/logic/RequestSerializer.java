914267f23c9cc5d41eb533fe97cc882f0704f191
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2022-11-08 21:18:04 -0500

    Feat: created chain classes
    Feat: add RequestStrategy Enum
    
    RequestStrategy represents a request type (ie what information we want to get, and what information we provide with the request)

---
+++ ExtraLeaderboard_EJB/src/main/java/com/extraleaderboard/logic/RequestSerializer.java
@@ -0,0 +1,27 @@
+package com.extraleaderboard.logic;
+
+import com.extraleaderboard.model.Request;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class RequestSerializer extends JsonSerializer<Request> {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void serialize(Request request,
+                          JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider)
+                          throws IOException {
+
+        StringWriter writer = new StringWriter();
+        mapper.writeValue(writer,request);
+        jsonGenerator.writeFieldName(writer.toString());
+
+    }
+}
