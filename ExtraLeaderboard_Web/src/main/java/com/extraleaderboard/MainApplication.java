4b1d1077acbf6e4f0bd0aa626c538d324f502b32
Banalian <64379456+Banalian@users.noreply.github.com>
2022-12-06 21:01:06 -0500

    feat(token): add live service token generation at app startup

--- ExtraLeaderboard_Web/src/main/java/com/extraleaderboard/MainApplication.java
+++ ExtraLeaderboard_Web/src/main/java/com/extraleaderboard/MainApplication.java
@@ -1,9 +1,18 @@
 package com.extraleaderboard;
 
+import com.extraleaderboard.logic.token.TokenFactory;
+import com.extraleaderboard.model.nadeo.Audience;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class MainApplication extends Application {
 
+    public MainApplication() {
+        super();
+
+        // Workaround : Create the needed token
+        TokenFactory.getToken(Audience.NADEO_LIVE_SERVICES);
+    }
 }
