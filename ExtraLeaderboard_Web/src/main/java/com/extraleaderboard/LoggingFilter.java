c05cb03533a7ae34d453672dd55e4b5297bed1bc
Banalian <64379456+Banalian@users.noreply.github.com>
2022-12-12 19:00:52 -0500

    feat(logging): add logging filter

---
+++ ExtraLeaderboard_Web/src/main/java/com/extraleaderboard/LoggingFilter.java
@@ -0,0 +1,46 @@
+package com.extraleaderboard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public void filter(ContainerRequestContext reqContext) {
+        long requestStartTime = System.nanoTime();
+        reqContext.setProperty("requestStartTime", requestStartTime);
+
+        // Create random request id
+        UUID requestId = UUID.randomUUID();
+        reqContext.setProperty("requestId", requestId);
+
+        // Log that a request has been received to X endpoint
+        logger.info("Started request {} to {}. Parameters : {}", requestId, reqContext.getUriInfo().getPath(), reqContext.getUriInfo().getQueryParameters());
+
+    }
+
+    @Override
+    public void filter(ContainerRequestContext reqContext,
+                       ContainerResponseContext resContext) {
+        long requestStartTime = (long) reqContext.getProperty("requestStartTime");
+        long requestFinishTime = System.nanoTime();
+        long duration = requestFinishTime - requestStartTime;
+
+        UUID requestId = (UUID) reqContext.getProperty("requestId");
+
+        // Print the elapsed time for the request
+        logger.info("Finished request {} to {} in {} ms", requestId, reqContext.getUriInfo().getPath(), TimeUnit.NANOSECONDS.toMillis(duration));
+    }
+
+}
